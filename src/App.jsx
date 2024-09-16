8a3bb2a3b353a7d70b65b2842fab179ce7b16893
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-15 23:24:51 -0500

    Refactorización de rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
-import { EmployeePages } from "./pages/EmployeePages";
-import {NewEmployeePage} from "./pages/NewEmployeePage"
-import {EditEmployeePage} from "./pages/EditEmployeePage"
+import { NewEmployeePage } from "./pages/NewEmployeePage";
+import { EditEmployeePage } from "./pages/EditEmployeePage";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/employees" element={<EmployeePages/>}/>
-      <Route path="/create" element={ <NewEmployeePage/> } />
-      <Route  path="/edit-employee/:id" element={<EditEmployeePage/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<NewEmployeePage />} />
+        <Route path="/edit-employee/:id" element={<EditEmployeePage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
